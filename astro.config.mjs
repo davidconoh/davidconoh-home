6fa8aad5c9068192080ef78a857806ce382d369f
David Onoh <davidconoh@live.com>
2022-10-08 04:33:59 +0100

    blog migrated from hugo _v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
@@ -8,7 +7,6 @@ export default defineConfig({
   // Resolves to the "./foo" directory in your current working directory
   root: '.',
   sitemap: true,
-  site: 'https://davidconoh.github.io',
   // base: '/my-repo',
   integrations: [preact(), svelte()],
   // Resolves to the "./foo/public" directory in your current working directory
